4dcc200e78bf25b75b8ce06039c6bc887b4b2b7b
shinoshino00 <onuki513@yahoo.co.jp>
2023-08-31 11:45:10 +0900

    Update main.js

--- Apps/main.js
+++ Apps/main.js
@@ -2,5 +2,5 @@ var viewer = new Cesium.Viewer('cesiumContainer');
 
 var czmlDataSource = new Cesium.CzmlDataSource();
 viewer.dataSources.add(czmlDataSource);
-czmlDataSource.load('./Apps/data/test.czml');
+czmlDataSource.load('./date/test.czml');
 
